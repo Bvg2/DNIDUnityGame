bf40c80e3764888081fbbd29ce13ca4d4f4d7b6f
Bvg2 <bvg2@pitt.edu>
2024-10-30 20:26:36 -0400

    Update camFollow.cs

--- Assets/_scripts/camFollow.cs
+++ Assets/_scripts/camFollow.cs
@@ -11,6 +11,7 @@ public class camFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         transform.position = player.transform.position + new Vector3(0, distY, -distZ);
     }
 }
